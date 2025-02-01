8260f070b51c420c74566156497e383a590f26d9
Lydia <Lcarter200183@yahoo.com>
2025-01-31 18:47:36 -0600

    added files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 
 const root = createRoot(window.bodyTag);
- root.render(<main></main>);
+ root.render(
+ <main>
+    <h3>hello world</h3>
+ </main>);
    
 
 
